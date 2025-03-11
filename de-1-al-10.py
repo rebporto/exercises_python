a4bc0a4acf158577f20609295125d532ac7f5c86
Renata <renataporto@me.com>
2025-03-11 16:02:46 -0300

    add new counting exercises

---
+++ de-1-al-10.py
@@ -0,0 +1,8 @@
+print(
+    "---------------------------contador de 1 ao 10 -------------------------------------"
+)
+
+contador = 0
+while contador <= 10:
+    contador += 1
+    print("el contador es:", contador)
