bba15296f6fb4b97c04553880dceb61bf7932923
Renata <renataporto@me.com>
2025-03-11 16:00:25 -0300

    add par o impar exercises

---
+++ par-o-impar.py
@@ -0,0 +1,9 @@
+print(
+    "---------------------------numero par o impar -------------------------------------"
+)
+number = input("choose a number from 0 to 1000: ")
+number = int(number)
+if number % 2 == 0:
+    print("seu numero es par")
+else:
+    print("seu numero é impar")
